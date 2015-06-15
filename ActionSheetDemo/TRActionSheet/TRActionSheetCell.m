af531737621390bffc713c746bf6bf1d3e1d40ad
frankxiongsir <frankxiongsir@users.noreply.github.com>
2015-06-15 11:50:21 +0800

    Update TRActionSheetCell.m
    change the label and line width

--- ActionSheetDemo/TRActionSheet/TRActionSheetCell.m
+++ ActionSheetDemo/TRActionSheet/TRActionSheetCell.m
@@ -20,16 +20,15 @@
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self)
     {
-        self.otherButtonLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 0, IPHONE_WIDTH - 60, 44)];
+        self.otherButtonLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 290 * self.frame.size.width / 320, 44)];
         self.otherButtonLabel.font = [UIFont systemFontOfSize:14.0f];
         self.otherButtonLabel.numberOfLines = 0;
         self.otherButtonLabel.lineBreakMode = NSLineBreakByCharWrapping;
         self.otherButtonLabel.textAlignment = NSTextAlignmentCenter;
         [self addSubview:self.otherButtonLabel];
-        self.seperateline = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0,IPHONE_WIDTH, 1)];
+        self.seperateline = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 290 * self.frame.size.width / 320, 1)];
         self.seperateline.backgroundColor = [UIColor colorWithRed:224.0/255.0 green:224.0/255.0 blue:224.0/255.0 alpha:1.0];
         [self.contentView addSubview:self.seperateline];
     }
-    return self;
-}
+    return self;}
 @end
